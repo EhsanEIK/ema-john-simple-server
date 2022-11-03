b8cb895d0e9ec5b55eb3fb77bc6078c568dc4f40
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-03 08:23:42 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+require('dotenv').config();
+
 // middleware
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fbieij7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.get('/', async (req, res) => {
     res.send('ema-john server is running');
