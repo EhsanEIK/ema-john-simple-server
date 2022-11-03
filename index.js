d1b96a83fb97e128147274e273e2173a7dcd5235
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-03 09:14:16 +0600

    get the count of total data from database

--- index.js
+++ index.js
@@ -8,9 +8,27 @@ require('dotenv').config();
 
 // middleware
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fbieij7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const productsCollection = client.db('emaJohnDB').collection('products');
+
+        // products
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            const count = await productsCollection.estimatedDocumentCount();
+            res.send({ count, products });
+        })
+    }
+    finally { }
+}
+run().catch(error => console.error(error));
 
 
 app.get('/', async (req, res) => {
